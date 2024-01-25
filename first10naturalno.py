a532ce8221e96c608593c335b47b0c087f59ae22
Preetam singh <156574938+Preetam4062@users.noreply.github.com>
2024-01-25 18:50:06 +0530

    Add files via upload

---
+++ first10naturalno.py
@@ -0,0 +1,8 @@
+sum_of_natural_numbers=0
+
+for n in range(1,11):
+    print("sum of the number",n)
+    sum_of_natural_numbers += n
+
+
+print("The sum of the first 10 natural numbers is:", sum_of_natural_numbers)
